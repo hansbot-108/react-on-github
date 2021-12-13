a2fc4593e8b6fb6cca40bef44a6dcf0a08f74e81
Krystian Dziopa <krystian.dziopa@ilustratum.pl>
2021-12-13 16:58:57 +0100

    Zmiana tytułu i linka Aplikacji

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Deploy Aplikacja <code>React</code> na GitHub'ie.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React On GitHub
         </a>
       </header>
     </div>
